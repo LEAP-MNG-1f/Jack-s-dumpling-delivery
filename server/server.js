256f82f03f21f695e84ad0d7f27a4eeb9ff6c4de
ligthman <jamka888@gmail.com>
2024-11-19 12:39:43 +0800

    backend upload image

--- server/server.js
+++ server/server.js
@@ -2,16 +2,34 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import { config } from "dotenv";
+import cloudinary from "cloudinary";
 config();
 
 const server = express();
 const PORT = 8000 || process.env.DB_URL;
-
+const cloudinaryURL = new URL(process.env.CLOUDINARY_URL);
+cloudinary.config({
+  cloud_name: cloudinaryURL.hostname,
+  api_key: cloudinaryURL.username,
+  api_secret: cloudinaryURL.password,
+});
 server.use(cors());
 server.use(bodyParser.json());
 
 server.get("/", (_, res) => {
   res.status(200).json("API server is working");
+});
+
+server.post("/image", async (_, res) => {
+  try {
+    const uploudResult = await cloudinary.uploader.upload("assets/egg.png", {
+      public_id: "cook",
+    });
+    res.status(200).json({ success: true, file: uploudResult.url });
+    console.log(uploudResult);
+  } catch (error) {
+    console.log("error", error);
+  }
 });
 
 server.listen(PORT, () => {
