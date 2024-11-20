2e27c498841b5b427f59529307f4b0ff2cc4e11f
Жаминжамц Батсундуй <24LP6854@administrators-iMac.local>
2024-11-20 13:40:59 +0800

    drawer

--- web/src/app/_features/Cart.tsx
+++ web/src/app/_features/Cart.tsx
@@ -1,3 +1,48 @@
-export default function Cart(){
-  return <main></main>
+import * as React from 'react';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+
+
+type Anchor = 'top' | 'left' | 'bottom' | 'right';
+
+export default function AnchorTemporaryDrawer() {
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer =
+    (anchor: Anchor, open: boolean) =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === 'keydown' &&
+        ((event as React.KeyboardEvent).key === 'Tab' ||
+          (event as React.KeyboardEvent).key === 'Shift')
+      ) {
+        return;
+      }
+
+      setState({ ...state, [anchor]: open });
+    };
+
+;
+
+  return (
+    <div>
+      {([ 'right'] as const).map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+          >
+          <p>haaaadknfdfro'sgnaerp'odgnervi</p>
+          </Drawer>
+        </React.Fragment>
+      ))}
+    </div>
+  );
 }
