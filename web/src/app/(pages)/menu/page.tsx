c1d34b5e274f3f12866327005ca2a456673f95dc
Жаминжамц Батсундуй <24LP6854@administrators-iMac-3.local>
2024-11-22 14:11:05 +0800

    mongodb

--- web/src/app/(pages)/menu/page.tsx
+++ web/src/app/(pages)/menu/page.tsx
@@ -1,6 +1,6 @@
 import { CategorySection, FoodListSection } from "@/app/_features";
 
-export default function Page() {
+export default function Menupage() {
   return (
     <main className="w-screen flex flex-col items-center justify-center gap-16">
       <CategorySection />
